41c4df27f6cb7e11b1b158a00340d8a71cd49ac9
Gregory LeClaire Wagner <wagner.greg@gmail.com>
2017-04-28 15:55:23 -0400

    Rewrites main script to account for new preprocessing function

--- nestedModelMaker.m
+++ nestedModelMaker.m
@@ -3,6 +3,7 @@ clear all
 % Define the names of both the parent model, and of child model to be built.
 child.name = 'BrazilBasin_LLC1080';
 
+% - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 % Initialize the script by copying code to active directory and adding the path.
 if exist('./active') == 7, eval('!rm ./active/*')
 else,                      mkdir('./active'),       end
@@ -12,19 +13,11 @@ eval(['!cp ./models/' child.name '/*.m ./active/'])
 
 addpath('./active/')
 
-% Directory to high-res bathymetry.
-bathyName  = 'SandS14p1_ibcao_4320x56160.bin';
-dirz.plotBathy = ['/data5/glwagner/Numerics/nestedModelMaker/bathymetry/' bathyName ];
-
-% Call user-defined functions - - - - - - - - - - - - - - - - - - - - - - - - - 
-% Specify properties of the parent model.
-[dirz, parent] = specifyParentProperties(dirz);
+% - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+% Initialization
+[dirz, parent] = specifyParentProperties();
 [dirz, child] = specifyChildProperties(dirz, child);
-
-checkDirectories(dirz)
-
-% Zoom-factor between child- and parent-grid resolution.
-child.zoom = child.res / parent.res;
+[dirz, parent, child] = checkAndProcessInput(dirz, parent, child);
 
 % Get open boundary conditions and grid info  - - - - - - - - - - - - - - - - - 
 % Parse parent data structure for open boundary information.
@@ -57,7 +50,6 @@ child = modifyBathymetry(child);
 child = discardUnconnectedOcean(child);
 
 [childObij, childObuv] = snapOpenBoundaryToSuperGrid(childObij, childObuv, child);
-
 childObij = getOpenBoundaryHorizontalGrid(dirz.childGlobalGrids, child, childObij);
 
 parent = getDomainGrid(dirz.parentGlobalGrids, parent);
@@ -84,34 +76,3 @@ tiling(child);
 
 % print obcs setup
 obcsSetup(child, childObij);
-
-% Next... - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-% 1. Figure out how to write data, data.obcs, data.exch2, etc.
-
-% Plot  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-for iOb = 1:child.nOb
-
-    [ii, jj] = getOpenBoundaryIndices(parentObij{iOb}, 'local', parent.offset);
-
-    % Plot bathymetry on the LLC grid with open boundary marked.
-    fig = 3; visualizeOpenBoundary(dirz.plotBathy, parentObij{iOb}, fig)
-
-    % Make a quick movie
-    %fig = 4; quickOpenBoundaryMovie(parentObuv{iOb}, parentObij{iOb}, child.nObcMonths, fig)
-    fig = 4; quickOpenBoundaryMovie(childObuv{iOb}, childObij{iOb}, child.nObcMonths, fig)
-
-end
-
-% Copy user-defined functions into model directory when script is complete.
-%eval(['!cp -r ' pwd '/user ' dirz.child.home])
-
-%{
-% Compare bathymetry along parent and child open boundaries.
-figure(2), clf, hold on
-plot(1/2 + [0:parentObij{iOb}.nn-1], parentObij{iOb}.depth1, 'k-')
-plot(1/child.zoom + [0:1/child.zoom:parentObij{iOb}.nn-1/child.zoom], childObij{iOb}.depth1, 'r-')
-xlabel('kkp'), ylabel('depth'), legend('parent grid', 'child grid')
-title(sprintf('Open boundary on the %s edge of face %d', childObij{iOb}.edge, childObij{iOb}.face))
-
-pause(0.1)
-%}
