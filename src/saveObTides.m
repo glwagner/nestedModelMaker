d0bbce439e3206c0d19ab3175a1c3dc3a1354b13
Gregory LeClaire Wagner <wagner.greg@gmail.com>
2017-05-01 11:42:34 -0400

    Changes style slightly but otherwise leaves unchanged

--- src/saveObTides.m
+++ src/saveObTides.m
@@ -1,44 +1,59 @@
 function saveObTides(dirz, child, childObij, childObTides)
 
-[nconst, l] = size(childObTides{1}.am_u);
+% This function saves tidal amplitudes and phases along open boundaries
+% in the format required by MITgcm and its LLC grid convention. This 
+% convention concatenates all open boundaries for each grid-direction
+% (rather than physical direction)
+
+[nconst, ~] = size(childObTides{1}.am_u);
 
 for edge = {'west', 'east', 'south', 'north'}
-  % initialize boundary fields with zeros
-  if strcmp(edge, 'west') | strcmp(edge, 'east')
-    n = sum(child.njj);
-  else
-    n = sum(child.nii);
-  end
-  am = zeros(nconst, n);
-  ph = zeros(nconst, n);
-  % look for open boundaries and fill in values
-  for face = 1:5
-    for ib = 1:length(childObij)
-      if childObij{ib}.face == face & strcmp(childObij{ib}.edge, edge)
-        if strcmp(edge, 'west') | strcmp(edge, 'east')
-          i0 = sum(child.njj(1:face-1))+1;
-          i1 = sum(child.njj(1:face));
-          am(:,i0:i1) = childObTides{ib}.am_u;
-          ph(:,i0:i1) = childObTides{ib}.ph_u;
-        else
-          i0 = sum(child.nii(1:face-1))+1;
-          i1 = sum(child.nii(1:face));
-          am(:,i0:i1) = childObTides{ib}.am_v;
-          ph(:,i0:i1) = childObTides{ib}.ph_v;
-        end
-      end
-    end
-  end
-  % save amplitude
-  file = sprintf('OB%cam_%dx%d.bin', upper(edge{1}(1)), n, nconst);
-  f = fopen([dirz.childInput, file], 'w', 'ieee-be');
-  fwrite(f, am', 'real*4');
-  fclose(f);
-  % save phase
-  file = sprintf('OB%cph_%dx%d.bin', upper(edge{1}(1)), n, nconst);
-  f = fopen([dirz.childInput, file], 'w', 'ieee-be');
-  fwrite(f, ph', 'real*4');
-  fclose(f);
-end
 
+    % Initialize boundary fields with zeros
+    switch edge{:}
+        case {'west', 'east'}
+            n = sum(child.njj);
+        case {'north', 'south'}
+            n = sum(child.nii);
+    end
+
+    am = zeros(nconst, n);
+    ph = zeros(nconst, n);
+
+    % Look for open boundaries and fill in non-zero values
+    for face = 1:5
+        for ib = 1:length(childObij)
+
+            if childObij{ib}.face == face & strcmp(childObij{ib}.edge, edge)
+
+                switch edge{:}
+                    case {'west', 'east'}
+                        i0 = sum(child.njj(1:face-1))+1;
+                        i1 = sum(child.njj(1:face));
+      
+                        am(:, i0:i1) = childObTides{ib}.am_u;
+                        ph(:, i0:i1) = childObTides{ib}.ph_u;
+                    case {'north', 'south'}
+                        i0 = sum(child.nii(1:face-1))+1;
+                        i1 = sum(child.nii(1:face));
+      
+                        am(:, i0:i1) = childObTides{ib}.am_v;
+                        ph(:, i0:i1) = childObTides{ib}.ph_v;
+                 end
+
+            end
+        end
+    end
+
+    % Save amplitude
+    filename = sprintf('OB%cam_%dx%d.bin', upper(edge{1}(1)), n, nconst);
+    file = fopen([dirz.childInput, filename], 'w', 'ieee-be');
+    fwrite(file, am', 'real*4');
+    fclose(file);
+
+    % Save phase
+    filename = sprintf('OB%cph_%dx%d.bin', upper(edge{1}(1)), n, nconst);
+    file = fopen([dirz.childInput, filename], 'w', 'ieee-be');
+    fwrite(file, ph', 'real*4');
+    fclose(file);
 end
